5ea1c06a77140472f8dbd553e3289a5671b554d8
Frank  D'Angelo <08fdange@gmail.com>
2020-02-24 15:59:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
     
   def self.create_from_filename(file_name) 
     song = self.new
-    file = file_name_split(" - ")
+    file = file_name.split(" - ")
     song.artist_name = file[0]
     song.name = file[1].chomp ".mp3"
     @@all << song
